4f92cb5f76546bf9adba393fe254de7cad812573
Aleksandra <ag7756862@gmail.com>
2023-01-03 23:53:24 +0200

    feat: solution tasks 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = e => localStorage.setItem('videoplayer-current-time', e.seconds);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+currentTime ? player.setCurrentTime(currentTime) : null;
+
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+import localStorageService from './localstorage.js';
+const {setInLocalStorage, getFromLocalStorage, removeFromLocalStorage} = localStorageService;
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const fillFeedbackFormFields = () => {
+  const feedbackInfoFromLS = getFromLocalStorage('userFeedback');
+
+  if (feedbackInfoFromLS === undefined) {
+    return;
+  }
+
+  for (const prop in feedbackInfoFromLS) {
+    feedbackFormEl.elements[prop].value = feedbackInfoFromLS[prop];
+  }
+};
+
+fillFeedbackFormFields();
+
+const onContactFormFieldChange = e => {
+  const { target } = e;
+
+  const fieldValue = target.value;
+  const fieldName = target.name;
+
+  userInfo[fieldName] = fieldValue;
+
+  setInLocalStorage('userFeedback', userInfo);
+};
+
+const onContactFormSubmit = e => {
+  e.preventDefault();
+
+  feedbackFormEl.reset();
+  removeFromLocalStorage('userFeedback');
+};
+
+feedbackFormEl.addEventListener('input', throttle(onContactFormFieldChange, 500));
+feedbackFormEl.addEventListener('submit', onContactFormSubmit);
 
